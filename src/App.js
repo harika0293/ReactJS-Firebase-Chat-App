68209430d376a76a96949da821a5b2eed0dd3e1c
Harika Rachamalla <92622089+harika0293@users.noreply.github.com>
2022-12-19 19:09:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route
-            path='/ReactJS-Firebase-Chat-App/'
+            path='/chat'
             element={
               <ProtectedRoute>
                 <DrawerContextProvider>
